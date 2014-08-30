20b5e1d5f398323a0d5110057c4015f4817f0619
nealfennimore <hi@neal.codes>
2014-08-30 18:09:42 -0400

    Main Controller is blank and MapController has test map_markers json

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,2 +1,18 @@
 class MapController < ApplicationController
+
+  def markers 
+    p params
+    map_markers = MapMarkers.all.to_json
+    p map_markers 
+    p "*"*50
+    respond_to do |format|
+      if params[:callback]
+        format.js { render :json => {:phones => map_markers}, :callback => params[:callback] }
+      else
+        format.json { render json: {:phones => map_markers}}
+      end
+    end
+  end
+
+
 end
