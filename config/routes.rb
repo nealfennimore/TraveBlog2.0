c851e6e1c49a6270c31b91f8580838154a232e94
nealfennimore <hi@neal.codes>
2014-08-30 18:05:21 -0400

    Made all routes go to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'main/index'
-  root 'main#index'
+  root 'application#index'
+  get '*path' => 'application#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
