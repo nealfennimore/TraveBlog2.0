edcca7c446a0e6e88ed77bd00af310d3139bd48b
nealfennimore <hi@neal.codes>
2014-08-30 18:04:38 -0400

    Added MapMarker test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times {
+  MapMarkers.create({latitude: (37 + rand), longitude: -(122 + rand)})
+}
